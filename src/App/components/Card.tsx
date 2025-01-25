3ed7c9dfd8a52eb7d01e22f676a707f417e69c2a
Cassio Passos <passoscassio29@gmail.com>
2025-01-25 14:02:17 -0300

    novo produto

--- src/App/components/Card.tsx
+++ src/App/components/Card.tsx
@@ -1,22 +1,24 @@
 const Card = () => {
 	return(
-		<div className="bg-white p-2 w-60 ronded-2xl">
+		<div className="bg-white w-60 rounded-2xl p-2">
 
 		  <div>
-			  <img>
+			  <img src="http://localhost:5173/public/assets/Produtos/C.png">
 				</img>
 		  </div>
 
-		  <div>
-			  <div>
+		  <div className="p-4">
+			  <div className="flex justify-center items-center mb-2">
 					<h3>Name Product</h3>
 				</div>
 
-				<div>
+				<div className="flex justify-center items-center">
 					<span>Value Product</span>
 				</div>
 
 		  </div>
+
+			<button className="w-full rounded-2xl bg-blue-500 py-2 px-4 hover:bg-blue-700 hover:text-white" >Adicionar no carrinho</button>
 
 		</div>
 	)
